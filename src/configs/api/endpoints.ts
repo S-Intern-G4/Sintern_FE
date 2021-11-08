e766de291c69d4e245a922ebd5fda93c251a0cdc
Vlad Goje <vlad.goje@evozon.com>
2021-11-08 13:04:52 +0200

    Step one working
    A new user can choose what kind of account wants to create
    
    # Conflicts:
    #	src/components/pages/Register/Register.tsx
    #	src/components/pages/Register/RegisterForm.tsx

--- src/configs/api/endpoints.ts
+++ src/configs/api/endpoints.ts
@@ -1,7 +1,7 @@
 export const ApiEndpoints = {
   companyRegister: '/companies/register',
   studentRegister: '/students/register',
-  login: '/api/user/login'
+  login: '/api/user/login',
   editUser: (id: string) => `/api/user/edit/${id}`,
   domains: '/domains',
   openPositions: '/open-positions/domain'
