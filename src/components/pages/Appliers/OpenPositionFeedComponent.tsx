ef15259165b88a0ce6a06564f3004eff547647c9
Manu-APO <manuelapostol20@gmail.com>
2021-12-06 13:13:37 +0200

    Add appliers view
    - CV view unfinished

--- src/components/pages/Appliers/OpenPositionFeedComponent.tsx
+++ src/components/pages/Appliers/OpenPositionFeedComponent.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Image } from 'antd';
-import { Document } from 'react-pdf';
+import React from "react";
+import styled from "styled-components";
+import { Document } from "react-pdf";
 
 const CardFeed = styled.div`
   width: 100%;
@@ -53,66 +52,52 @@ const DateOfSubmission = styled.div`
   font-size: 20px;
 `;
 
-const MyImage = styled(Image)`
-  margin: 20px 0px 20px 0px;
-`;
-
 const OpenPositionFeedComponent = (props) => {
-    const { studentApplier } = props;
+  const { studentApplier } = props;
 
-    //const classes = useStyles();
-
-    return (
-        <CardFeed>
-            <Document file={studentApplier.cv} />
-            <TextCard>
-                <StudentName>
-                    <strong>First name:</strong> {studentApplier.firstName}
-                </StudentName>
-                <StudentName>
-                    <strong>Last name:</strong> {studentApplier.lastName}
-                </StudentName>
-                <University>
-                    <strong>University: </strong>
-                    {studentApplier.educationDetails.university}
-                </University>
-                <Faculty>
-                    <strong>Faculty:</strong> {studentApplier.educationDetails.faculty}
-                </Faculty>
-                <Specialization>
-                    <strong>Specialization:</strong> {studentApplier.educationDetails.specialization}
-                </Specialization>
-                <YearOfStudy>
-                    <strong>Year Of Study:</strong>{' '}
-                    {studentApplier.educationDetails.yearOfStudy}
-                </YearOfStudy>
-                <PhoneNumber>
-                    <strong>Phone Number:</strong>{' '}
-                    {studentApplier.phoneNumber}
-                </PhoneNumber>
-                <Email>
-                    <strong>Email:</strong>{' '}
-                    {studentApplier.email}
-                </Email>
-                <DateOfBirth>
-                    <strong>Date Of Birth:</strong>{' '}
-                    {studentApplier.dateOfBirth}
-                </DateOfBirth>
-                <Description>
-                    <strong>Description:</strong>{' '}
-                    {studentApplier.description}
-                </Description>
-                <DateOfSubmission>
-                    <strong>Date Of Submission:</strong>{' '}
-                    {studentApplier.dateOfSubmission}
-                </DateOfSubmission>
-            </TextCard>
-        </CardFeed>
-    );
+  return (
+    <CardFeed>
+      <Document file={studentApplier.cv} />
+      <TextCard>
+        <StudentName>
+          <strong>First name:</strong> {studentApplier.firstName}
+        </StudentName>
+        <StudentName>
+          <strong>Last name:</strong> {studentApplier.lastName}
+        </StudentName>
+        <University>
+          <strong>University: </strong>
+          {studentApplier.educationDetails.university}
+        </University>
+        <Faculty>
+          <strong>Faculty:</strong> {studentApplier.educationDetails.faculty}
+        </Faculty>
+        <Specialization>
+          <strong>Specialization:</strong>{" "}
+          {studentApplier.educationDetails.specialization}
+        </Specialization>
+        <YearOfStudy>
+          <strong>Year Of Study:</strong>{" "}
+          {studentApplier.educationDetails.yearOfStudy}
+        </YearOfStudy>
+        <PhoneNumber>
+          <strong>Phone Number:</strong> {studentApplier.phoneNumber}
+        </PhoneNumber>
+        <Email>
+          <strong>Email:</strong> {studentApplier.email}
+        </Email>
+        <DateOfBirth>
+          <strong>Date Of Birth:</strong> {studentApplier.dateOfBirth}
+        </DateOfBirth>
+        <Description>
+          <strong>Description:</strong> {studentApplier.description}
+        </Description>
+        <DateOfSubmission>
+          <strong>Date Of Submission:</strong> {studentApplier.dateOfSubmission}
+        </DateOfSubmission>
+      </TextCard>
+    </CardFeed>
+  );
 };
 
 export default OpenPositionFeedComponent;
-function useStyles() {
-  throw new Error('Function not implemented.');
-}
-
--- src/components/pages/Appliers/OpenPositionFeedContainer.tsx
+++ src/components/pages/Appliers/OpenPositionFeedContainer.tsx
@@ -1,16 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const OpenPositionFeedContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
-  margin:0 auto;
+  margin: 0 auto;
   width: 70%;
-  @media (max-width:1000px){
-    width:95%;
-  };
-  overflow-y:scroll;
+  @media (max-width: 1000px) {
+    width: 95%;
+  }
+  overflow-y: scroll;
 `;
 
 export default OpenPositionFeedContainer;
